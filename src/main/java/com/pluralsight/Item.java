f3f60ebdd9d35ba9413f45b639097205c1523eee
Fernando <f.miranda25d@gmail.com>
2026-04-26 17:28:35 -0500

    Displays ledger options, and displays all entries (in no particular order for now)

---
+++ src/main/java/com/pluralsight/Item.java
@@ -0,0 +1,61 @@
+package com.pluralsight;
+
+public class Item {
+    private String date;
+    private String time;
+    private String itemDescription;
+    private String vendorName;
+    private double amount;
+
+    public Item() {
+
+    }
+
+    public Item(String date, String time, String itemDescription, String vendorName, double amount) {
+        this.date = date;
+        this.time = time;
+        this.itemDescription = itemDescription;
+        this.vendorName = vendorName;
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,50 +1,62 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Program {
     static Scanner input = new Scanner(System.in);
-
+    //ArrayList<Item> items = new ArrayList<Item>();
     public static void main(String[] args) {
-        boolean programIsRunning = true;
+//        ArrayList<Item> items = new ArrayList<Item>();
+        try {
+            FileReader fileReader = new FileReader("transactions.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        while (programIsRunning) {
-            System.out.println("""
-                    Welcome To The Accounting Ledger
-                    
-                    What would you like to do?
-                    D). Add deposit
-                    P). Make payment
-                    L). Display the ledger
-                    X). Exit the application
-                    
-                    Enter:
-                    """);
-            String command = input.nextLine();
-            command = command.toUpperCase();
-            switch (command) {
-                case "D":
-                    displayDepositScreen();
-                    break;
-                case "P":
-                    displayPaymentScreen();
-                    break;
-                case "L":
-                    displayLedgerScreen();
-                    break;
-                case "X":
-                    programIsRunning = false;
-                    break;
+            boolean programIsRunning = true;
+
+            while (programIsRunning) {
+                System.out.print("""
+                        Welcome To The Accounting Ledger
+                        
+                        What would you like to do?
+                        D). Add deposit
+                        P). Make payment
+                        L). Display the ledger
+                        X). Exit the application
+                        
+                        Enter: 
+                        """);
+                String command = input.nextLine();
+                command = command.toUpperCase();
+                switch (command) {
+                    case "D":
+                        displayDepositScreen();
+                        break;
+                    case "P":
+                        displayPaymentScreen();
+                        break;
+                    case "L":
+                        displayLedgerScreen();
+                        break;
+                    case "X":
+                        programIsRunning = false;
+                        break;
+                }
+
             }
-
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
     } ////////////////////////////////////////////////////////////////////////////////////////////////////// end of main
 
     public static void displayDepositScreen() {
-        System.out.println("Hello 1");
+        System.out.print("Enter the amount you would like to deposit: ");
+        double depositAmount = input.nextDouble();
+        System.out.println(depositAmount);
     }
 
     public static void displayPaymentScreen() {
@@ -52,8 +64,82 @@ public class Program {
     }
 
     public static void displayLedgerScreen() {
-        System.out.println("Hello 3");
+        System.out.println("""
+                Here is a list of options
+                
+                What would you like to do?
+                A) All - Display all entries
+                D) Deposits - Display only the entries that are deposits into the account
+                P) Payments - Display only the negative entries (or payments)
+                R) Reports - 
+                H) Return to home screen
+                
+                Enter: 
+                """);
+        String ledgerScreenCommand = input.nextLine();
+        ledgerScreenCommand = ledgerScreenCommand.toUpperCase();
+        switch (ledgerScreenCommand) {
+            case "A":
+                displayAllLedgerEntries();
+                break;
+            case "D":
+                displayLedgerDeposits();
+                break;
+            case "P":
+                displayLedgerPayments();
+                break;
+            case "R":
+                displayLedgerReports();
+                break;
+            case "H":
+                returnToHomeScreen();
+                break;
+        }
+    } /////////////////////////////////////////////////////////////////////////////////////// end of displayLedgerScreen
+
+    public static void displayAllLedgerEntries() { ///// Bedtime stories
+        try {
+            FileReader fileReader = new FileReader("transactions.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = bufferedReader.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+
+            System.out.println("Press H to return");
+            String whereAfterAllEntries = input.nextLine();
+            whereAfterAllEntries = whereAfterAllEntries.toUpperCase();
+            if (whereAfterAllEntries.equals("H")) {
+                displayLedgerScreen();
+            } else {
+                displayAllLedgerEntries();
+            }
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    public static void displayLedgerDeposits() {
+
+    }
+
+    public static void displayLedgerPayments() {
+
+    }
+
+    public static void displayLedgerReports() {
+
+    }
+    public static void returnToHomeScreen() {
+        System.out.println("");
+    }
+//    public static ArrayList<Item> getItems() {
+//        return new ArrayList<>();
+//    }
 
 
 }
