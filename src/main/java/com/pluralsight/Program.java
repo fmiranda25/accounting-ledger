a8d3de49f5fa2fb449088d027b65ee264f8ab6ca
Fernando <f.miranda25d@gmail.com>
2026-04-26 11:06:24 -0500

    Displays barebones versions of the child screens and contains a transactions.csv file

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,4 +1,59 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean programIsRunning = true;
+
+        while (programIsRunning) {
+            System.out.println("""
+                    Welcome To The Accounting Ledger
+                    
+                    What would you like to do?
+                    D). Add deposit
+                    P). Make payment
+                    L). Display the ledger
+                    X). Exit the application
+                    
+                    Enter:
+                    """);
+            String command = input.nextLine();
+            command = command.toUpperCase();
+            switch (command) {
+                case "D":
+                    displayDepositScreen();
+                    break;
+                case "P":
+                    displayPaymentScreen();
+                    break;
+                case "L":
+                    displayLedgerScreen();
+                    break;
+                case "X":
+                    programIsRunning = false;
+                    break;
+            }
+
+        }
+    } ////////////////////////////////////////////////////////////////////////////////////////////////////// end of main
+
+    public static void displayDepositScreen() {
+        System.out.println("Hello 1");
+    }
+
+    public static void displayPaymentScreen() {
+        System.out.println("Hello 2");
+    }
+
+    public static void displayLedgerScreen() {
+        System.out.println("Hello 3");
+    }
+
+
 }
